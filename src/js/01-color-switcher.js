873029ea9961e3ba2d8bdbb10044c2619ee24590
Wojciech <46322266+Wojtas71@users.noreply.github.com>
2024-03-06 22:00:42 +0100

    first test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  const body = document.body;
+  let intervalId = null;
+  
+  function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+  }
+  
+  startBtn.addEventListener('click', () => {
+    intervalId = setInterval(changeBackgroundColor, 1000); 
+    startBtn.disabled = true; 
+    stopBtn.disabled = false; 
+  });
+  
+  stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId); 
+    startBtn.disabled = false; 
+    stopBtn.disabled = true; 
+  });
