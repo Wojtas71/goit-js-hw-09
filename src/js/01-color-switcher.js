77f5b7059ff3acabda9039ccb59072bfc9573f03
Wojciech <46322266+Wojtas71@users.noreply.github.com>
2024-03-07 19:29:43 +0100

    Third try

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,20 +5,20 @@ function getRandomHexColor() {
   const startBtn = document.querySelector('[data-start]');
   const stopBtn = document.querySelector('[data-stop]');
   const body = document.body;
-  let intervalId = null;
+
   
   function changeBackgroundColor() {
     body.style.backgroundColor = getRandomHexColor();
+    setTimeout(changeBackgroundColor, 1000);
   }
   
-  startBtn.addEventListener('click', () => {
-    intervalId = setInterval(changeBackgroundColor, 1000); 
+  startBtn.addEventListener('click', function() {
     startBtn.disabled = true; 
     stopBtn.disabled = false; 
+    changeBackgroundColor();
   });
   
-  stopBtn.addEventListener('click', () => {
-    clearInterval(intervalId); 
+  stopBtn.addEventListener('click', function() {
     startBtn.disabled = false; 
     stopBtn.disabled = true; 
   });
